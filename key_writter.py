d774453e9ed39e17f934c5d20da56fa3352a1dd6
Daniel Bodnár <50233772+LittlepawD@users.noreply.github.com>
2021-02-10 21:24:34 +0100

    Update key_writter.py

--- key_writter.py
+++ key_writter.py
@@ -5,7 +5,7 @@ Only run this if your api keys have changed or keys.bin is missing.
 import pickle
 
 # keys are not real - see your own api tokens and insert here. 
-keys = ("820678802:AAF-Yre6KKDJZyeqi9EYjMmsd2WS1nr2TiY","empty","empty")
+keys = ("empty","empty","empty")
 
 with open("keys.bin", "wb") as f:
     pickle.dump(keys, f)
