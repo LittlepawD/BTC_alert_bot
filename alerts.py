5bdb5aaf0ba19765ee21bcb33eb152e3080a1445
LittlepawD <dbodnr37@gmail.com>
2021-02-11 11:49:04 +0100

    make bot global, move alert funs to alerts.py

--- alerts.py
+++ alerts.py
@@ -1,3 +1,7 @@
+import pickle
+
+ALERTS_FILE = "alerts_dic.bin"
+
 class Alert:
     def __init__(self, id, owner, price = None, notify = None) -> None:
         self.id = id
@@ -7,7 +11,23 @@ class Alert:
         self.was_notified = False
 
     def __str__(self) -> str:
-        return f"Alert {self.id}: Notify {self.notify} {self.price} EUR."
+        return f"Alert {self.id}: Notify {self.owner} {self.notify} {self.price} EUR."
 
     def __repr__(self) -> str:
-        return self.id
+        return f"Alert {self.id} for {self.owner}"
+
+def load_alerts(file = ALERTS_FILE):
+    with open(file, "rb") as f:
+        alerts = pickle.load(f)
+    return alerts
+
+def save_alerts(alerts, file=ALERTS_FILE):
+    with open(file, "wb") as f:
+        pickle.dump(alerts, f)
+    
+
+def check_alerts(alerts):
+    pass
+
+if __name__ == '__main__':
+    save_alerts({})
--- coinbot.py
+++ coinbot.py
@@ -1,9 +1,8 @@
-from alerts import Alert
+from alerts import *
 import multiprocessing as mp
 
 import telebot, pickle, time
 
-ALERTS_FILE = "alerts_dic.bin"
 
 with open("keys.bin", "rb") as f:       
     T_KEY, B_KEY, C_KEY = pickle.load(f)
@@ -11,48 +10,51 @@ with open("keys.bin", "rb") as f:
 class CoinBot (telebot.TeleBot):
     def __init__(self, token):
         super().__init__(token)
+        self.alerts = load_alerts()
 
 
-def define_bot(lock):
-    bot = CoinBot(T_KEY)
+bot = CoinBot(T_KEY)
 
+
+@bot.message_handler(commands = ["start", "help"])
+def handle_start_help(message):
     help_text = "this is a help text"
+    bot.reply_to(message, help_text)
 
-    @bot.message_handler(commands = ["start", "help"])
-    def handle_start_help(message):
-        bot.reply_to(message, help_text)
+@bot.message_handler(commands = ["alerts"])
+def handle_alerts(message):
+    bot.reply_to(message, "alerts")
 
-    @bot.message_handler(commands = ["alerts"])
-    def handle_alerts(message):
-        bot.reply_to(message, "alerts")
-
-    @bot.message_handler(commands = ["set_alert"])
-    def handle_set_alert(message):
-        pass
-
-    @bot.message_handler(commands = ["remove_alert"])
-    def handle_remove_alert(message):
-        pass
-
-    return bot
-
-def bot_start(bot):
-    bot.polling()
-
-def check_alerts():
+@bot.message_handler(commands = ["set_alert"])
+def set_alert(message):
     pass
 
+@bot.message_handler(commands = ["remove_alert"])
+def handle_remove_alert(message):
+    pass
+
+
+def bot_start():
+    bot.polling()
+
 if __name__=='__main__':
+    # test alerts:
+    alerts = {}
+    alerts[14] = Alert(14, "danko", 1451, "yes")
+    save_alerts(alerts)
+
     lock = mp.Lock()
-    this_bot = define_bot(lock)
-    bot_thread = mp.Process(target=bot_start, args=(this_bot,))
+    bot_thread = mp.Process(target=bot_start)
     bot_thread.start()
+    print(bot.alerts)
+
 
     cont = True
     while cont:
         try:
             # Mainloop:
-            check_alerts(alerts, bot)
+            alerts = load_alerts()
+            check_alerts(alerts)
             print("checked")
             time.sleep(2)
             
